f2cef37547ad010acfb2c71baa4dead98a291a39
kraukskis <rolandsantulis07@gmail.com>
2023-11-24 14:30:33 +0200

    3.uzdevums izpildīts

---
+++ 3.uzdevums.py
@@ -0,0 +1,7 @@
+"""Izveidojiet Python programmu, kas pārbauda, vai ievadītais skaitlis ir nepāra, izmantojot if priekšrakstu.
+Izmantojiet GIT, lai izsekotu izmaiņas un veidotu komitus."""
+
+ievaditais_skaitlis=int(input("Ievadiet skaitli: "))
+
+if ievaditais_skaitlis / 2:
+    print(f'Ievadītais skaitlis ir pāra.')
